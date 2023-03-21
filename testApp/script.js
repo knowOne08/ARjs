ce075b4830504ba1e78071d57b2e853451aeaf1f
knowOne08 <yashdarji23082004@gmail.com>
2023-03-21 15:13:02 +0530

    Testing

--- testApp/script.js
+++ testApp/script.js
@@ -1,85 +1,104 @@
-window.onload = () => {
-    const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
-
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
-
-function staticLoadPlaces() {
-    return [
-        {
-            name: 'Pokèmon',
-            location: {
-                lat: 23.106811046788337,
-                lng: 72.59442981374761,
+// getting places from APIs
+function loadPlaces(position) {
+    const method = 'static';
+    // const method = 'api';
+    if(method === 'static') {
+        return [
+            {
+                name: "LIBRARY",
+                location: {
+                    lat: 23.108156404203143, // add here latitude if using static data
+                    lng: 72.59498734978379, // add here longitude if using static data
+                }
             },
-        },
-    ];
-}
-
-var models = [
-    {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
-    {
-        url: './assets/articuno/scene.gltf',
-        scale: '0.2 0.2 0.2',
-        rotation: '0 180 0',
-        info: 'Articuno, Lv. 80, HP 100/100',
-    },
-    {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
-        info: 'Dragonite, Lv. 99, HP 150/150',
-    },
-];
-
-var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
+            {
+                name: "A-block",
+                location: {
+                    lat: 23.1064422997269,  // add here latitude if using static data
+                    lng: 72.59575386356772, // add here longitude if using static data
+                }
+            },
+            {
+                name: "M-Block",
+                location: {
+                    lat: 23.10796558905296, // add here latitude if using static data
+                    lng:  72.59461530062512, // add here longitude if using static data
+                }
+            },
+            {
+                name: "GTU",
+                location: {
+                    lat: 23.105923398933385,  // add here latitude if using static data
+                    lng: 72.59414710034639, // add here longitude if using static data
+                }
+            },
+        ];
+    } else {
+        const params = {
+            radius: 300,    // search places not farther than this value (in meters)
+            clientId: '<your-client-id>',
+            clientSecret: '<your-client-secret>',
+            version: '20300101',    // foursquare versioning, required but unuseful for this demo
+        };
+    
+        // CORS Proxy to avoid CORS problems
+        const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    
+        // Foursquare API (limit param: number of maximum places to fetch)
+        const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+            &ll=${position.latitude},${position.longitude}
+            &radius=${params.radius}
+            &client_id=${params.clientId}
+            &client_secret=${params.clientSecret}
+            &limit=30 
+            &v=${params.version}`;
+        return fetch(endpoint)
+            .then((res) => {
+                return res.json()
+                    .then((resp) => {
+                        return resp.response.venues;
+                    })
+            })
+            .catch((err) => {
+                console.error('Error with places API', err);
+            })
     }
-
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
-
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
-
-    entity.setAttribute('gltf-model', model.url);
-
-    const div = document.querySelector('.instructions');
-    div.innerText = model.info;
+    
 };
 
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
 
-        setModel(models[modelIndex], model);
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
 
-        model.setAttribute('animation-mixer', '');
+                    // add place name
+                    const placeText = document.createElement('a-link');
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('title', place.name);
+                    placeText.setAttribute('scale', '15 15 15');
+                    
+                    placeText.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
 
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
-
-        scene.appendChild(model);
-    });
-}
+                    scene.appendChild(placeText);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
