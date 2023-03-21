cf9a65ece42a2751300817f7b2d4f2a4e814d741
knowOne08 <yashdarji23082004@gmail.com>
2023-03-21 13:44:03 +0530

    Testing

--- ar/index.js
+++ ar/index.js
@@ -1,64 +1,64 @@
-const loadPlaces = function (coords) {
-    // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
-    // const method = 'api';
+// getting places from APIs
+function loadPlaces(position) {
 
+    // const method = 'api'
+    
     const PLACES = [
         {
-            name: "VGEC LIBRARY",
+            name: "LIBRARY",
             location: {
-                lat: 23.108064062630472, // add here latitude if using static data
-                lng: 72.59511008934776,// add here longitude if using static data
-            },
-            name: "M Block Road",
+                lat: 23.108156404203143, // add here latitude if using static data
+                lng: 72.59498734978379, // add here longitude if using static data
+            }
+        },
+        {
+            name: "A-block",
             location: {
-                lat: 23.107965095162964, // add here latitude if using static data
-                lng:  72.59462781943799,// add here longitude if using static data
-            },
-            name: "A block Front",
+                lat: 23.1064422997269,  // add here latitude if using static data
+                lng: 72.59575386356772, // add here longitude if using static data
+            }
+        },
+        {
+            name: "M-Block",
             location: {
-                lat: 23.1063519237275, // add here latitude if using static data
-                lng:   72.59578565947602,// add here longitude if using static data
-            },
+                lat: 23.10796558905296, // add here latitude if using static data
+                lng:  72.59461530062512, // add here longitude if using static data
+            }
         },
     ];
 
-    if (method === 'api') {
-        return loadPlaceFromAPIs(coords);
+    if(method == 'api') {
+        const params = {
+            radius: 300,    // search places not farther than this value (in meters)
+            clientId: '<YOUR-CLIENT-ID>',
+            clientSecret: 'YOUR-CLIENT-SECRET',
+            version: '20300101',    // foursquare versioning, required but un-      useful for this demo
+        };
+    
+        // CORS Proxy to avoid CORS problems
+        const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    
+        // Foursquare API (limit param: number of maximum places to fetch)
+        const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+            &ll=${position.latitude},${position.longitude}
+            &radius=${params.radius}
+            &client_id=${params.clientId}
+            &client_secret=${params.clientSecret}
+            &limit=30 
+            &v=${params.version}`;
+        return fetch(endpoint)
+            .then((res) => {
+                return res.json()
+                    .then((resp) => {
+                        return resp.response.venues;
+                    })
+            })
+            .catch((err) => {
+                console.error('Error with places API', err);
+            })
+    } else {
+        return Promise.resolve(PLACES);
     }
-
-    return Promise.resolve(PLACES);
-};
-
-// getting places from REST APIs
-function loadPlaceFromAPIs(position) {
-    const params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
-        clientSecret: 'QWT2HBMQ1LUC4BYQHZWO2UQNEEANJENUIMYBG4JH32AC1OGA',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API
-    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=15
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
 };
 
 
@@ -76,17 +76,16 @@ window.onload = () => {
                     const longitude = place.location.lng;
 
                     // add place name
-                    const text = document.createElement('a-link');
-                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    text.setAttribute('title', place.name);
-                    text.setAttribute('href', 'http://www.example.com/');
-                    text.setAttribute('scale', '20 20 20');
-
-                    text.addEventListener('loaded', () => {
+                    const placeText = document.createElement('a-link');
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('title', place.name);
+                    placeText.setAttribute('scale', '15 15 15');
+                    
+                    placeText.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
                     });
 
-                    scene.appendChild(text);
+                    scene.appendChild(placeText);
                 });
             })
     },
